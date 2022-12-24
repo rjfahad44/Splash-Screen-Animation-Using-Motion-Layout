01341b975f343429476a13ee9b398651ba144cbc
Fahad_2022 <rjfahad44@gmail.com>
2022-12-24 18:46:01 +0600

    ..

---
+++ app/src/main/java/com/ft/splash_screen_animation_using_motion_layout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ft.splash_screen_animation_using_motion_layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/ft/splash_screen_animation_using_motion_layout/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.ft.splash_screen_animation_using_motion_layout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            finish()
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 2000)
+    }
+}
